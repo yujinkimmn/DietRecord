a3a85996db4a557da7457c641b4d65434d567d9a
yujinkimmn <rqwuy8@naver.com>
2021-12-18 02:46:36 +0900

    [feat] db 검색 기능 구현

--- app/src/main/java/com/example/dietrecord/food/frag_search.kt
+++ app/src/main/java/com/example/dietrecord/food/frag_search.kt
@@ -1,9 +1,13 @@
 package com.example.dietrecord.food
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +16,7 @@ import com.example.dietrecord.R
 import com.example.dietrecord.TAG_HOME_FRAGMENT
 import com.example.dietrecord.databinding.SearchBinding
 import com.example.dietrecord.menu.frag_home
+import com.google.common.primitives.UnsignedBytes.toInt
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.food.view.*
 
@@ -44,8 +49,25 @@ class frag_search : Fragment(){
 
         // 검색
         binding.btnFragSearch.setOnClickListener {
+            Toast.makeText(context, "검색하기", Toast.LENGTH_SHORT).show()
             (binding.rvFragSearch.adapter as RecyclerViewAdapter).searchFood(binding.etFragSearch.text.toString())
         }
+
+        // 엔터키 이벤트 처리(엔터키 눌러도 검색 되게)
+        binding.etFragSearch.setOnKeyListener { v, keyCode, keyEvent ->
+//            var handled = false
+//            if (keyCode == EditorInfo.IME_ACTION_DONE){
+//                binding.btnFragSearch.performClick()
+//                handled = true
+//            }
+//            handled
+            if (keyEvent.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER){
+                Toast.makeText(context, "검색하기", Toast.LENGTH_SHORT).show()
+                (binding.rvFragSearch.adapter as RecyclerViewAdapter).searchFood(binding.etFragSearch.text.toString())
+            }
+            true
+        }
+
         return binding.root
     }
 
@@ -83,8 +105,8 @@ class frag_search : Fragment(){
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
             var viewHolder = (holder as ViewHolder).itemView
             viewHolder.tv_food_name.text = foodList[position].foodName
-            viewHolder.tv_food_gram.text = foodList[position].gram
-            viewHolder.tv_frag_food_calorie.text = foodList[position].calorie
+            viewHolder.tv_food_gram.text = (foodList[position].gram + "g")
+            viewHolder.tv_frag_food_calorie.text = (foodList[position].calorie + "kcal")
         }
 
         override fun getItemCount(): Int {
@@ -93,15 +115,15 @@ class frag_search : Fragment(){
 
         // 파이어스토어에서 음식 검색하기 함수
         fun searchFood(searchWord: String){
-            firestore?.collection("nutrients")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-
+            firestore?.collection("nutrients_ver3")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
                 // ArrayList 비우기
                 foodList.clear()
 
                 for (snapshot in querySnapshot!!.documents){
-                    if(snapshot.contains(searchWord)){
+                    if(snapshot.getString("foodName")!!.contains(searchWord)){
                         var item = snapshot.toObject(Food::class.java)
                         foodList.add(item!!)
+
                     }
                 }
                 notifyDataSetChanged()
